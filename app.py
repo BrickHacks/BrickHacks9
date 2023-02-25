6a5b109fac2a0dd261a403cca842c2715879a7ef
adc2108 <adc2108@rit.edu>
2023-02-25 14:56:29 -0500

    website tings

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to my website!'
+    return render_template('home.html')
+
 
 if __name__ == '__main__':
     app.run()

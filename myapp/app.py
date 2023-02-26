2194de44698b686e1f1aba2f706018aabb2be0cc
adc2108 <adc2108@rit.edu>
2023-02-26 07:14:03 -0500

    Game works no point system yet

--- myapp/app.py
+++ myapp/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, Response, render_template
+import random
+from flask import Flask, Response, render_template, request
 import cv2
 import ffmpeg
 from SignScanner import get_video_stream, getPrediction
@@ -7,7 +8,6 @@ from twilio.rest import Client
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from pytz import timezone
-from game import index
 
 app = Flask(__name__)
 
@@ -33,9 +33,61 @@ def dashboard_page():
     print('levels')
     return render_template("levels.html")
 
+animals = {
+	"A": "/css/letters/A.png",
+	"B": "/css/letters/B.png",
+	"C": "/css/letters/C.png",
+    "D": "/css/letters/D.png",
+    "E": "/css/letters/E.png",
+    "F": "/css/letters/F.png",
+    "G": "/css/letters/G.png",
+    "H": "/css/letters/H.png",
+    "I": "/css/letters/I.png",
+    "J": "/css/letters/J.png",
+    "K": "/css/letters/K.png",
+    "L": "/css/letters/L.png",
+    "M": "/css/letters/M.png",
+    "N": "/css/letters/N.png",
+    "O": "/css/letters/O.png",
+    "P": "/css/letters/P.png",
+    "Q": "/css/letters/Q.png",
+    "R": "/css/letters/R.png",
+    "S": "/css/letters/S.png",
+    "T": "/css/letters/T.png",
+    "U": "/css/letters/U.png",
+    "V": "/css/letters/V.png",
+    "W": "/css/letters/W.png",
+    "X": "/css/letters/X.png",
+    "Y": "/css/letters/Y.png",
+    "Z": "/css/letters/z.png"
+}
+
 @app.route('/alphabet')
-def alphabet_page():
-    index()
+def index():
+    print('here')
+    # Get a random animal and its image filename
+    animal, image_filename = random.choice(list(animals.items()))
+
+    return render_template('alphabet.html', image_filename=image_filename, animal=animal)
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    # Get the guessed animal from the form
+    guessed_animal = request.form['guess']
+
+    # Get the actual animal from the form
+    actual_animal = request.form['animal']
+
+    if guessed_animal.lower() == actual_animal.lower():
+        message = "Congratulations, you guessed it!"
+    else:
+        message = "Sorry, that's not the right answer. Try again!"
+
+    # Get a new random animal and its image filename
+    animal, image_filename = random.choice(list(animals.items()))
+
+    return render_template('alphabet.html', image_filename=image_filename, animal=animal, message=message)
+
 
 
 def sendMessage():
